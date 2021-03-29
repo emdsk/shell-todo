c858433fc96c877a1916697b6c846d8377b718ca
Kannan <mail.kannandhanasekaran@gmail.com>
2021-03-29 20:57:07 +0530

    update welcome message

--- shell-todo.py
+++ shell-todo.py
@@ -1,1 +1,13 @@
-print("hello world")
+def main():
+    print("----------------------------------------------------------------------------")
+    print("")
+    print("                         Shell Todo Welcomes you")
+    print("")
+    print("----------------------------------------------------------------------------")
+    print("")
+    print("                 The Best Place To Maintain All Your Tasks")
+    print("")
+
+
+
+main()
